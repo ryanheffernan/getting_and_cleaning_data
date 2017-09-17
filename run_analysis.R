90d04427b2f6dcd6c9771da90304e229a1f16c2a
Ryan Heffernan <ryanheffernan@fb.com>
2017-09-17 16:56:29 -0700

    Removing meanfreq columns

--- run_analysis.R
+++ run_analysis.R
@@ -19,19 +19,20 @@ test_data <- data.table(read.table('UCI HAR Dataset/test/X_test.txt'))
 names(test_data) <- variable_names
 test_data[,activity:=as.factor(readLines('UCI HAR Dataset/test/y_test.txt'))]
 test_data[,subject:=as.factor(readLines('UCI HAR Dataset/test/subject_test.txt'))]
-test_data[, which(duplicated(names(test_data))) := NULL]
+test_data[,which(duplicated(names(test_data))) := NULL]
 
 train_data <- data.table(read.table('UCI HAR Dataset/train/X_train.txt'))
 names(train_data) <- variable_names
 train_data[,activity:=as.factor(readLines('UCI HAR Dataset/train/y_train.txt'))]
 train_data[,subject:=as.factor(readLines('UCI HAR Dataset/train/subject_train.txt'))]
 train_data[,!duplicated(names(train_data))]
-train_data[, which(duplicated(names(train_data))) := NULL]
+train_data[,which(duplicated(names(train_data))) := NULL]
 
 tidy_data <- select(
     rbind(test_data, train_data),
     contains('_std'),
     contains('_mean'),
+    -contains('_meanfreq'),
     'activity',
     'subject'
 )
