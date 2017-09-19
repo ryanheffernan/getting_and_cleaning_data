e394c30adf2cbb5fb1c63d11e830460fa2bb34f2
Ryan Heffernan <ryanheffernan@fb.com>
2017-09-18 19:52:22 -0700

    Refactored to avoid code duplication

--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,55 @@
 library(data.table)
 library(dplyr)
 
-label_preamble <- '^[0-9 ]+'
-activity_labels <- gsub(
-    label_preamble,
-    '', 
-    readLines('UCI HAR Dataset/activity_labels.txt')
+loadUCIDataset <- function(dataFile, labelFile, subjectFile) {
+    labelPreamble <- '^[0-9 ]+'
+    activityLabels <- gsub(
+        labelPreamble,
+        '', 
+        readLines('UCI HAR Dataset/activity_labels.txt')
+    )
+    variableNames <- gsub(
+        labelPreamble,
+        '',
+        readLines('UCI HAR Dataset/features.txt')
+    )
+    
+    data <- data.table(read.table(dataFile))
+    names(data) <- variableNames
+    
+    data[,activity:=as.factor(readLines(labelFile))]
+    data[,subject:=as.factor(readLines(subjectFile))]
+    data[,which(duplicated(names(data))) := NULL]
+    
+    levels(data$activity) <- activityLabels
+    
+    data <- select(
+        data,
+        contains('std()'),
+        contains('mean()'),
+        'activity', 
+        'subject'
+    )
+    names(data) <- gsub('[\\,\\-]', '_', names(data))
+    names(data) <- gsub('[\\(\\)]', '', names(data))
+    
+    return(data)
+}
+
+
+
+testData <- loadUCIDataset(
+    'UCI HAR Dataset/test/X_test.txt', 
+    'UCI HAR Dataset/test/y_test.txt', 
+    'UCI HAR Dataset/test/subject_test.txt'
 )
-variable_names <- tolower(gsub(
-    label_preamble,
-    '',
-    readLines('UCI HAR Dataset/features.txt')
-))
-variable_names <- gsub('[\\,\\-]', '_', variable_names)
-variable_names <- gsub('[\\(\\)]', '', variable_names)
-
-test_data <- data.table(read.table('UCI HAR Dataset/test/X_test.txt'))
-names(test_data) <- variable_names
-test_data[,activity:=as.factor(readLines('UCI HAR Dataset/test/y_test.txt'))]
-test_data[,subject:=as.factor(readLines('UCI HAR Dataset/test/subject_test.txt'))]
-test_data[,which(duplicated(names(test_data))) := NULL]
-
-train_data <- data.table(read.table('UCI HAR Dataset/train/X_train.txt'))
-names(train_data) <- variable_names
-train_data[,activity:=as.factor(readLines('UCI HAR Dataset/train/y_train.txt'))]
-train_data[,subject:=as.factor(readLines('UCI HAR Dataset/train/subject_train.txt'))]
-train_data[,!duplicated(names(train_data))]
-train_data[,which(duplicated(names(train_data))) := NULL]
-
-tidy_data <- select(
-    rbind(test_data, train_data),
-    contains('_std'),
-    contains('_mean'),
-    -contains('_meanfreq'),
-    'activity',
-    'subject'
+trainData <- loadUCIDataset(
+    'UCI HAR Dataset/train/X_train.txt', 
+    'UCI HAR Dataset/train/y_train.txt', 
+    'UCI HAR Dataset/train/subject_train.txt'
 )
-levels(tidy_data$activity) <- activity_labels
 
-write.table(tidy_data, 'tidy_data.txt')
+allData <- rbind(testData, trainData)
+
+
+write.table(allData, 'tidy_data.txt')
